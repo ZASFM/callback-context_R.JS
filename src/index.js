359b6a97bf5fe6a08b42d9063be27f99bfbcbbcc
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-11-15 11:41:39 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+//import './index.css'
+import App from './App'
+ReactDOM.render(
+   <StrictMode>
+      <Router>
+         <App/>
+      </Router>
+   </StrictMode>,
+   document.getElementById('root')
+)
